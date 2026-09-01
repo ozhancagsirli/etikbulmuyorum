1585e74c84ffa2d37471264e35659a054893a8ff
Ozhan Cagsirli <ozhancagsirli@Ozhan-Mac-mini.local>
2026-09-02 00:16:28 +0300

    instagram alanlari select e eklendi

--- backend/src/routes/incidents.js
+++ backend/src/routes/incidents.js
@@ -51,7 +51,7 @@ router.get('/', optionalAuth, async (req, res, next) => {
     const sql = `
       SELECT i.id, i.title, i.description, i.location, i.is_anonymous,
         i.vote_ethical, i.vote_unethical, i.view_count, i.created_at,
-        i.images, i.tags, i.subject, i.person_name, i.profession, i.trust_score, (SELECT avatar_url FROM subjects WHERE name = i.subject LIMIT 1) AS subject_avatar,
+        i.images, i.tags, i.subject, i.person_name, i.instagram_username, i.instagram_avatar, i.instagram_verified, i.instagram_followers, i.trust_score, (SELECT avatar_url FROM subjects WHERE name = i.subject LIMIT 1) AS subject_avatar,
         c.slug AS category_slug, c.name_tr AS category_name, c.icon AS category_icon,
         CASE WHEN i.is_anonymous THEN NULL ELSE u.name END AS author_name,
         CASE WHEN i.is_anonymous THEN NULL ELSE u.avatar_url END AS author_avatar,
@@ -84,7 +84,7 @@ router.get('/:id', optionalAuth, async (req, res, next) => {
     const { rows } = await pool.query(`
       SELECT i.id, i.title, i.description, i.location, i.incident_date, i.is_anonymous,
         i.vote_ethical, i.vote_unethical, i.view_count, i.status, i.created_at,
-        i.images, i.tags, i.subject, i.person_name, i.profession, i.trust_score, (SELECT avatar_url FROM subjects WHERE name = i.subject LIMIT 1) AS subject_avatar,
+        i.images, i.tags, i.subject, i.person_name, i.instagram_username, i.instagram_avatar, i.instagram_verified, i.instagram_followers, i.trust_score, (SELECT avatar_url FROM subjects WHERE name = i.subject LIMIT 1) AS subject_avatar,
         c.slug AS category_slug, c.name_tr AS category_name, c.icon AS category_icon,
         CASE WHEN i.is_anonymous THEN NULL ELSE u.name END AS author_name,
         CASE WHEN i.is_anonymous THEN NULL ELSE u.avatar_url END AS author_avatar,
